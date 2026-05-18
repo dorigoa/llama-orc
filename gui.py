5a61732bf5bb3bd35aaac4d6f18ac01134e5ecac
dorigoa <alvise72@live.com>
2026-05-18 15:32:44 +0200

    fix

--- gui.py
+++ gui.py
@@ -722,9 +722,14 @@ with ui.column().classes("w-full max-w-4xl mx-auto p-4 gap-4"):
 
         with ui.row().classes("w-full gap-4 mt-4 items-end"):
 
+            models available_model_names()
+            last_started = utils.load_last_launched_model()
+            if not last_started:
+                last_started = next(iter(models)
+            
             model_select = ui.select(
-                options=available_model_names(),#next(iter(available_model_names()), None),
-                value=utils.load_last_launched_model() | next(iter(available_model_names()), None),
+                options=models,
+                value=last_started,
                 label="Select a model from the list below...",
                 on_change=lambda _: update_data_from_model(),
             ).classes("flex-1")
