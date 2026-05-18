51ec3e9daff1d4a69e8c665c43c2102f9a6cb252
dorigoa <alvise72@live.com>
2026-05-18 15:34:26 +0200

    fix

--- gui.py
+++ gui.py
@@ -722,7 +722,7 @@ with ui.column().classes("w-full max-w-4xl mx-auto p-4 gap-4"):
 
         with ui.row().classes("w-full gap-4 mt-4 items-end"):
 
-            models available_model_names()
+            models = available_model_names()
             last_started = utils.load_last_launched_model()
             if not last_started:
                 last_started = next(iter(models)
